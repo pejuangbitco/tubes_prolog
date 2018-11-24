fe706df6e7a6ef58ebe08b25a1cf0a57c5674852
muhammad irsyad masyhudin <irsyadeinstein@gmail.com>
2018-11-24 14:24:44 +0700

    add

---
+++ prolog.js
@@ -0,0 +1,8 @@
+const jalan = () => {
+	let input = document.getElementById("inputan").value
+	document.getElementById("hasil").innerHTML = input
+	
+	input = input.split(" ")
+	console.log(input)
+}
+
